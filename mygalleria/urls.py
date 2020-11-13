60e097fbb741ee8efcf5c3997b17b9f763d33cee
Nyash-Mauro <mauricee423@gmail.com>
2020-11-13 22:53:01 +0300

    added urls to the application

--- mygalleria/urls.py
+++ mygalleria/urls.py
@@ -2,3 +2,10 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path
 from . import views
+
+urlpatterns=[
+    path('',views.index,name='index'),
+    path(r'archives/(\d{4}-\d{2}-\d{2})/', views.past_days_photos, name = 'pastPhotos'),    
+    path(r'search/', views.search_results, name='search_results'),
+    path(r'category/(\d+)',views.category,name ='category')
+]
