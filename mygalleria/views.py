0350eff9cd977114621930584652289886aea996
Nyash-Mauro <mauricee423@gmail.com>
2020-11-14 11:36:15 +0300

    added a function to get upload pics from cloudinary

--- mygalleria/views.py
+++ mygalleria/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django import forms
+from django.http import HttpResponse
 
-# Create your views here.
+from cloudinary.forms import cl_init_js_callbacks      
+from .models import Photo
+from .forms import PhotoForm
+
+def upload(request):
+  context = dict( backend_form = PhotoForm())
+
+  if request.method == 'POST':
+    form = PhotoForm(request.POST, request.FILES)
+    context['posted'] = form.instance
+    if form.is_valid():
+        form.save()
+
+  return render(request, 'upload.html', context)
