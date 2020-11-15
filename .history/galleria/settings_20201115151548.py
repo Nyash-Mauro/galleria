d5758c31c5d813adb6ba84b8ea19fe53b80f686a
Nyash-Mauro <mauricee423@gmail.com>
2020-11-15 16:34:59 +0300

    made changes to navbar

--- .history/galleria/settings_20201115151548.py
+++ .history/galleria/settings_20201115151548.py
@@ -56,8 +56,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware'
-]
+    'whitenoise.middleware.WhiteNoiseMiddleware'c]
 
 ROOT_URLCONF = 'galleria.urls'
 
